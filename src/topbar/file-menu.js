832027f055c8e6bf97088c84592c4ff8a6eaef62
PlanetGit <46271095+PlanetGit@users.noreply.github.com>
2022-12-30 23:19:00 +0900

    clean codebase up

--- src/topbar/file-menu.js
+++ src/topbar/file-menu.js
@@ -10,8 +10,6 @@ import {
   MenuDivider,
 } from '@blueprintjs/core';
 import { Popover2 } from '@blueprintjs/popover2';
-import FaFileExport from '@meronex/icons/fa/FaFileExport';
-import FaFileImport from '@meronex/icons/fa/FaFileImport';
 import { downloadFile } from 'polotno/utils/download';
 
 export const FileMenu = observer(({ store, project }) => {
@@ -23,7 +21,6 @@ export const FileMenu = observer(({ store, project }) => {
       <Popover2
         content={
           <Menu>
-            {/* <MenuDivider title={t('toolbar.layering')} /> */}
             <MenuItem
               icon="plus"
               text="Create new design"
@@ -44,18 +41,8 @@ export const FileMenu = observer(({ store, project }) => {
                 project.save();
               }}
             />
-            {/* {project.id !== 'local' && (
-              <MenuItem
-                icon="duplicate"
-                text="Make a copy"
-                onClick={() => {
-                  project.duplicate();
-                }}
-              />
-            )} */}
             <MenuDivider />
             <MenuItem
-              // icon={<FaFileImport />}
               icon="folder-open"
               text="Open"
               onClick={() => {
@@ -63,7 +50,6 @@ export const FileMenu = observer(({ store, project }) => {
               }}
             />
             <MenuItem
-              // icon={<FaFileExport />}
               icon="floppy-disk"
               text="Save as"
               onClick={() => {
@@ -75,7 +61,7 @@ export const FileMenu = observer(({ store, project }) => {
                     unescape(encodeURIComponent(JSON.stringify(json)))
                   );
 
-                downloadFile(url, 'polotno.json');
+                downloadFile(url, 'canvas.json');
               }}
             />
 
@@ -130,7 +116,7 @@ export const FileMenu = observer(({ store, project }) => {
       <Dialog
         icon="info-sign"
         onClose={() => toggleFaq(false)}
-        title="About Polotno Studio"
+        title="About Canvas Studio"
         isOpen={faqOpened}
         style={{
           width: '80%',
@@ -138,45 +124,16 @@ export const FileMenu = observer(({ store, project }) => {
         }}
       >
         <div className={Classes.DIALOG_BODY}>
-          <h2>What is Polotno Studio?</h2>
+          <h2>Contact Us</h2>
           <p>
-            <strong>Polotno Studio</strong> - is a web application to create
-            graphical designs. You can mix image, text and illustrations to make
-            social media posts, youtube previews, podcast covers, business cards
-            and presentations.
-          </p>
-          <h2>Is it Open Source?</h2>
-          <p>
-            Partially. The source code is available in{' '}
-            <a href="https://github.com/lavrton/polotno-studio" target="_blank">
-              GitHub repository
-            </a>
-            . The repository doesn't have full source.{' '}
-            <strong>Polotno Studio</strong> is powered by{' '}
+            Canvas Studio created by <a href="https://www.peopleperhour.com/site/register?rfrd=5560900.5" target="_blank">
+            Vladislav Novikov
+            </a>{' '}
+            and is powered by{' '}
             <a href="https://polotno.dev/" target="_blank">
               Polotno SDK project
             </a>
-            . All core "canvas editor" functionality are implemented by{' '}
-            <strong>polotno</strong> npm package (which is not open source at
-            the time of writing this text).
-          </p>
-          <p>
-            Polotno Studio is build on top of Polotno SDK to provide a
-            desktop-app-like experience.
-          </p>
-          <h2>Who is making Polotno Studio?</h2>
-          <p>
-            My name is Anton Lavrenov{' '}
-            <a href="https://twitter.com/lavrton" target="_blank">
-              @lavrton
-            </a>
-            . I am founder of Polotno project. As the maintainer of{' '}
-            <a href="https://konvajs.org/" target="_blank">
-              Konva 2d canvas framework
-            </a>
-            , I created several similar apps for different companies around the
-            world. So I decided to compile all my knowledge and experience into
-            reusable Polotno project.
+            .
           </p>
         </div>
         <div className={Classes.DIALOG_FOOTER}>
